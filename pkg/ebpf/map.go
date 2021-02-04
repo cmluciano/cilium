cmluciano/cilium

commit a18f01d7c991619c5c4a82762248c7919a8699a6

Add initial ebpf package implementation
This package is a wrapper around cilium/ebpf. It currently implements a
minimal set of the bpf package (while still relying on that) to allow
the migration of the maps/metricsmap package to cilium/ebpf.

Signed-off-by: Gilberto Bertin <gilberto@isovalent.com>

pkg/ebpf/map.go
@@ -1,0 +1,114 @@
+// Copyright 2021 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ebpf
+
+import (
+	"fmt"
+
+	"github.com/cilium/cilium/api/v1/models"
+	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/lock"
+
+	ciliumebpf "github.com/cilium/ebpf"
+)
+
+type MapSpec = ciliumebpf.MapSpec
+
+const (
+	PerCPUHash = ciliumebpf.PerCPUHash
+
+	PinByName = ciliumebpf.PinByName
+)
+
+// IterateCallback represents the signature of the callback function expected by
+// the IterateWithCallback method, which in turn is used to iterate all the
+// keys/values of a map.
+type IterateCallback func(key, value interface{})
+
+// Map represents an eBPF map.
+type Map struct {
+	lock lock.RWMutex
+	*ciliumebpf.Map
+
+	spec *MapSpec
+	path string
+}
+
+// NewMap creates a new Map object.
+func NewMap(spec *MapSpec) *Map {
+	return &Map{
+		spec: spec,
+	}
+}
+
+// OpenOrCreate tries to open or create the eBPF map identified by the spec in
+// the Map object.
+func (m *Map) OpenOrCreate() error {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	if m.Map != nil {
+		return nil
+	}
+
+	opts := ciliumebpf.MapOptions{
+		PinPath: bpf.MapPrefixPath(),
+	}
+
+	newMap, err := ciliumebpf.NewMapWithOptions(m.spec, opts)
+	if err != nil {
+		return fmt.Errorf("unable to create map: %w", err)
+	}
+
+	m.Map = newMap
+	m.path = bpf.MapPath(m.spec.Name)
+
+	registerMap(m)
+	return nil
+}
+
+// IterateWithCallback iterates through all the keys/values of a map, passing
+// each key/value pair to the cb callback.
+func (m *Map) IterateWithCallback(key, value interface{}, cb IterateCallback) error {
+	if m.Map == nil {
+		if err := m.OpenOrCreate(); err != nil {
+			return err
+		}
+	}
+
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	entries := m.Iterate()
+	for entries.Next(key, value) {
+		cb(key, value)
+	}
+
+	return nil
+}
+
+// GetModel returns a BPF map in the representation served via the API.
+func (m *Map) GetModel() *models.BPFMap {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	mapModel := &models.BPFMap{
+		Path: m.path,
+	}
+
+	// TODO: handle map cache. See pkg/bpf/map_linux.go:GetModel()
+
+	return mapModel
+}

daemon/cmd/map.go
@@ -18,6 +18,7 @@ import (
 	"github.com/cilium/cilium/api/v1/models"
 	restapi "github.com/cilium/cilium/api/v1/server/restapi/daemon"
 	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/ebpf"
 
 	"github.com/go-openapi/runtime/middleware"
 )
@@ -49,7 +50,7 @@ func NewGetMapHandler(d *Daemon) restapi.GetMapHandler {
 
 func (h *getMap) Handle(params restapi.GetMapParams) middleware.Responder {
 	mapList := &models.BPFMapList{
-		Maps: bpf.GetOpenMaps(),
+		Maps: append(bpf.GetOpenMaps(), ebpf.GetOpenMaps()...),
 	}
 
 	return restapi.NewGetMapOK().WithPayload(mapList)

pkg/ebpf/doc.go
@@ -1,0 +1,18 @@
+// Copyright 2021 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package ebpf provides functions that allow golang programs to interact with
+// ebpf maps by wrapping the cilium/ebpf library.
+// +groupName=pkg
+package ebpf

pkg/ebpf/ebpf.go
@@ -1,0 +1,24 @@
+// Copyright 2021 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ebpf
+
+import (
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+)
+
+var (
+	log = logging.DefaultLogger.WithField(logfields.LogSubsys, "bpf")
+)

pkg/ebpf/map_register.go
@@ -1,0 +1,68 @@
+// Copyright 2021 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ebpf
+
+import (
+	"path"
+
+	"github.com/cilium/cilium/api/v1/models"
+	"github.com/cilium/cilium/pkg/bpf"
+	"github.com/cilium/cilium/pkg/lock"
+)
+
+var (
+	mutex       lock.RWMutex
+	mapRegister = map[string]*Map{}
+)
+
+func registerMap(m *Map) {
+	mutex.Lock()
+	mapRegister[m.path] = m
+	mutex.Unlock()
+
+	log.WithField("path", m.path).Debug("Registered BPF map")
+}
+
+// GetMap returns the registered map with the given name or absolute path
+func GetMap(name string) *Map {
+	mutex.RLock()
+	defer mutex.RUnlock()
+
+	if !path.IsAbs(name) {
+		name = bpf.MapPath(name)
+	}
+
+	return mapRegister[name]
+}
+
+// GetOpenMaps returns a slice of all open BPF maps. This is identical to
+// calling GetMap() on all open maps.
+func GetOpenMaps() []*models.BPFMap {
+	// create a copy of mapRegister so we can unlock the mutex again as
+	// locking Map.lock inside of the mutex is not permitted
+	mutex.RLock()
+	maps := make([]*Map, 0, len(mapRegister))
+	for _, m := range mapRegister {
+		maps = append(maps, m)
+	}
+	mutex.RUnlock()
+
+	mapList := make([]*models.BPFMap, len(maps))
+	for i, m := range maps {
+		mapList[i] = m.GetModel()
+	}
+
+	return mapList
+}

commit 44508f00669ab599b2b42a12efa8169d345b0268

ebpf: Add missing map preallocate flags
If the agent is run with `--preallocate-bpf-maps=false`, then all
supported BPF maps will be created with BPF_F_NO_PREALLOC set. This
stores a 1 inside the github.com/cilium/ebpf.Map struct under the
`flags` field. The logic missing in ebpf.(*Map).OpenOrCreate()
(pkg/ebpf) can cause the agent to fatal after a restart (or an upgrade)
because this function will always try to open the map (as long as it
exists) with BPF_F_NO_PREALLOC _not_ set (which is 0), regardless of
whether the map was actually created with BPF_F_NO_PREALLOC:

```
level=debug msg="Registered BPF map" path=/sys/fs/bpf/tc/globals/cilium_ipcache subsys=bpf
level=error msg="Error while opening/creating BPF maps" error="unable to create map: use pinned map cilium_metrics: expected flags 0, got 1" subsys=daemon
level=fatal msg="Error while creating daemon" error="unable to create map: use pinned map cilium_metrics: expected flags 0, got 1" subsys=daemon
```

This commit fixes the above setting BPF_F_NO_PREALLOC properly.

Fixes: a18f01d7c9 ("Add initial ebpf package implementation")

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/ebpf/map.go
@@ -67,6 +67,9 @@ func (m *Map) OpenOrCreate() error {
 		PinPath: bpf.MapPrefixPath(),
 	}
 
+	mapType := bpf.GetMapType(bpf.MapType(m.spec.Type))
+	m.spec.Flags = m.spec.Flags | bpf.GetPreAllocateMapFlags(mapType)
+
 	newMap, err := ciliumebpf.NewMapWithOptions(m.spec, opts)
 	if err != nil {
 		return fmt.Errorf("unable to create map: %w", err)